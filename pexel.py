8af140f7527f823c116d537d393f08fc8ae2dd62
Gary Tang <garytang@Quris-MacBook-Pro-2.local>
2016-11-22 11:32:48 -0800

    updated api script

--- pexel.py
+++ pexel.py
@@ -1,8 +1,10 @@
+import json
 import os
 import requests
 
 KEY = os.environ["PEXEL_API_KEY"]
 headers = {"Authorization": KEY}
-url = "http://api.pexels.com/v1/search?query=dog&per_page=250&page=1"
+url = "http://api.pexels.com/v1/search?query=dog&per_page=250&page=2"
 r = requests.get(url, headers=headers)
-
+with open("image_dump/dogs/images.json", "w") as fw:
+    json.dump(r.json(), fw)
